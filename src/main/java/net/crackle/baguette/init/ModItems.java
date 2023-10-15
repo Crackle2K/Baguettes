ab6039accfa43cc9aa14bce40e6f3e5758b50b2d
Crackle2K <103861462+Crackle2K@users.noreply.github.com>
2023-10-15 08:52:28 -0400

    Update ModItems.java

--- src/main/java/net/crackle/baguette/init/ModItems.java
+++ src/main/java/net/crackle/baguette/init/ModItems.java
@@ -4,8 +4,6 @@ import net.crackle.baguette.Baguette;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterials;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -14,11 +12,10 @@ public class ModItems {
     public static Item BAGUETTE;
 
     public static void init() {
-        BAGUETTE = registerItem(new SwordItem(ToolMaterials.WOOD, 4, 0.6F, new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoods.BAGUETTE).maxCount(1)), "baguette");
+        BAGUETTE = registerItem(new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoods.BAGUETTE).maxCount(64)), "baguette");
     }
 
     public static Item registerItem(Item item, String name) {
         return Registry.register(Registry.ITEM, new Identifier(Baguette.MOD_ID, name), item);
     }
 }
-
